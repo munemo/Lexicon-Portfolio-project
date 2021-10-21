9423f664f6fd3f38df6459345e2643498da87ba6
marrycv <marrychriz@gmail.com>
2021-10-21 09:45:39 +0200

    project portfolio ground 0 first commit

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url 
+from basic_app import views
+from django.conf import settings
+from django.views.static import serve
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^portfolio/$', views.portfoliopage, name='portfolio'),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+]
