fdd74740d40565cf4cbb12384ae31d0e912f4729
marrycv <marrychriz@gmail.com>
2021-10-22 13:52:51 +0200

    branch testing ground 1 project files first commit

--- learning_users/basic_app/admin.py
+++ learning_users/basic_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from basic_app.models import UserProfileInfo
+from basic_app.models import UserProfileInfo, SubscriberInfo
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
+admin.site.register(SubscriberInfo)
--- learning_users/basic_app/forms.py
+++ learning_users/basic_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from basic_app.models import UserProfileInfo
+from basic_app.models import UserProfileInfo, SubscriberInfo
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -15,3 +15,7 @@ class UserProfileInfoForm(forms.ModelForm):
         model = UserProfileInfo
         fields = ('portfolio_site','profile_pic')
 
+class SubscriberInfoForm(forms.ModelForm):
+    class Meta():
+        model = SubscriberInfo
+        fields = ()
