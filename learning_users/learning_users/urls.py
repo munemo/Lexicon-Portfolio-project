7c585f4d0d8f78a637c0c44403646996f004bf7a
ccajlen <ccajlen@icloud.com>
2021-10-25 15:45:44 +0200

    Christoffer

--- learning_users/learning_users/urls.py
+++ learning_users/learning_users/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^basic_app/',include('basic_app.urls')),
     url(r'^logout/$',views.user_logout, name='logout'),
-    url(r'special/',views.special, name='special')
+    url(r'special/',views.special, name='special'),
 ]
 
 # Serving the media files in development mode
