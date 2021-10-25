a03c76919eebef6d82f21aa7f352d4ebd126ff78
munemo <john.chimbani@outlook.com>
2021-10-25 14:03:53 +0200

    modifyied forms.py

--- learning_users/basic_app/forms.py
+++ learning_users/basic_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db import models
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from basic_app.models import UserProfileInfo
+from basic_app.models import UserProfileInfo, SubscriberInfo 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -16,7 +16,11 @@ class UserProfileInfoForm(forms.ModelForm):
      
      class Meta():
         model = UserProfileInfo
-        fields = ('portfolio_site','profile_pic', 'description' )
+        fields = ('portfolio_site','profile_pic', 'description' ,'courses')
         
 
 
+class SubscriberInfoForm(forms.ModelForm):
+    class Meta():
+        model = SubscriberInfo
+        fields = ()
