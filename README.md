a4efeba09414fb851a0558ef7d7ae05850e113c0
neonalton <neonalton@gmail.com>
2021-10-26 10:11:18 +0200

    Merging latest Testing

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lexicon-Portfolio-project testing file
+Team project for Lexicon Django course
+
+- making change here...
+
+- Making some more changes // C
---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url 
+from basic_app import views
+from django.conf import settings
+from django.views.static import serve
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^portfolio/$', views.portfoliopage, name='portfolio'),
+    url(r'^list_profiles/$', views.list_profiles, name='list_profiles'),
+    url(r'^subscribe/$', views.subscribe, name='subscribe'),
+    url(r'^list_subscribers/$', views.list_subscribers, name='list_subscribers'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+]
