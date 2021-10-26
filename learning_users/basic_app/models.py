b738d5491c946a8479076875850c9e418cec4906
ccajlen <ccajlen@icloud.com>
2021-10-26 14:14:48 +0200

    Portfolio layout updates

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from multiselectfield import MultiSelectField
+from PIL import Image
 
 # Create your models here.
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User)
     portfolio_site = models.URLField(blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True, default="harry.jpeg")
     phone = models.CharField(max_length=200, default="+46 000 000 0000", blank=True)
 
     description = models.TextField(max_length=500, default="Profile Statement", blank=True)
@@ -18,6 +19,16 @@ class UserProfileInfo(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 200 or img.width > 200:
+            output_size = (200,200)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
 class SubscriberInfo(models.Model):
     subscriber_email = models.EmailField(null=True, blank=True, max_length=200, unique=True)
